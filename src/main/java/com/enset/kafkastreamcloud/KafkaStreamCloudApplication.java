f3db9aa182e2094115ecba433e5e8a34e0f72a5b
achrafhardizi <achrafhardizi1002@gmail.com>
2025-10-01 13:28:34 +0100

    init Project

---
+++ src/main/java/com/enset/kafkastreamcloud/KafkaStreamCloudApplication.java
@@ -0,0 +1,13 @@
+package com.enset.kafkastreamcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaStreamCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaStreamCloudApplication.class, args);
+    }
+
+}
