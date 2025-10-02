c8b194e55346e7b21844959af336144c2b6aaad7
achrafhardizi <achrafhardizi1002@gmail.com>
2025-10-02 12:11:59 +0100

    Added Analytics api endpoint, index.html to visualize realtime analytics

--- src/main/java/com/enset/kafkastreamcloud/handlers/PageEventHandler.java
+++ src/main/java/com/enset/kafkastreamcloud/handlers/PageEventHandler.java
@@ -1,11 +1,16 @@
 package com.enset.kafkastreamcloud.handlers;
 
 import com.enset.kafkastreamcloud.event.PageEvent;
+import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -39,6 +44,11 @@ public class PageEventHandler {
     public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
         return (input) ->
             input.filter((k,v) -> v.duration()>100)
-                    .map((k,v)-> new KeyValue<>(v.name(), v.duration()));
+                    .map((k,v)-> new KeyValue<>(v.name(), v.duration()))
+                    .groupByKey(Grouped.with(Serdes.String(), Serdes.Long()))
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                    .count(Materialized.as("count-store"))
+                    .toStream()
+                    .map((k,v)-> new KeyValue<>(k.key(), v));
     }
 }
