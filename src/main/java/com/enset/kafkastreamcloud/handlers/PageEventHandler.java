c5cdad0008921d73e72e427f12bf05fb9b0cf437
achrafhardizi <achrafhardizi1002@gmail.com>
2025-10-02 11:46:46 +0100

    Add PageEvent, PageEventController, PageEventHandler
    + Add PageEvent Record
    + Add Rest Controller that push https request arguments name and topic using StreamBuilder on Post request
    + Add PageEventHandler that has Consumer and aslo Supplier and kStream Function while defining input and outputs and in application.properties

---
+++ src/main/java/com/enset/kafkastreamcloud/handlers/PageEventHandler.java
@@ -0,0 +1,44 @@
+package com.enset.kafkastreamcloud.handlers;
+
+import com.enset.kafkastreamcloud.event.PageEvent;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Component
+public class PageEventHandler {
+
+    @Bean
+    public Consumer<PageEvent> pageEventConsumer(){
+        return (input) -> {
+            System.out.println("***************");
+            System.out.println(input.date());
+            System.out.println("***************");
+        };
+    }
+
+    @Bean
+    public Supplier<PageEvent> pageEventSupplier(){
+        return () -> {
+            return new PageEvent(
+                Math.random()>0.5?"P1":"P2",
+                Math.random()>0.5?"U1":"U2",
+                new Date(),
+                 10+new Random().nextInt(10000));
+        };
+    }
+
+    @Bean
+    public Function<KStream<String, PageEvent>, KStream<String, Long>> kStreamFunction(){
+        return (input) ->
+            input.filter((k,v) -> v.duration()>100)
+                    .map((k,v)-> new KeyValue<>(v.name(), v.duration()));
+    }
+}
